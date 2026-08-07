8e66a09afcfd66dc92980965f83e720831bf5cf4
selezenart <artvs14@gmail.com>
2026-08-07 17:48:53 +0200

    ex1 with no main

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -6,15 +6,19 @@ def spell(target: str, power: int) -> str:
 
 
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+    return lambda target, power: (spell1(target, power),
+                                  spell2(target, power))
 
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    ...
+    return lambda target, power: base_spell(target, power*multiplier)
 
 
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    ...
+    return lambda target, power: (spell(target, power)
+                                  if condition(target, power)
+                                  else "Spell fizzled")
 
 
 def spell_sequence(spells: list[Callable]) -> Callable:
-    ...
+    return lambda target, power: [spell(target, power) for spell in spells]
